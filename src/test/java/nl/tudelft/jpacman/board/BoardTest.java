ef1845b6832f3802f4f1f89ed288fd3653b8f159
planwis <witch2544@hotmail.co.th>
2022-09-18 21:46:35 +0700

    test: add more case to Assertions

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,4 +1,5 @@
 package nl.tudelft.jpacman.board;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,8 +14,24 @@ public class BoardTest {
     }
 
     @Test
-    void testSquareAt(){
+    void testBoardWithNullSquare(){
         Square[][] grid = new Square[1][1];
         Board board = new Board(grid);
+    }
+
+    @Test
+    void testSquareAtWithNormalInitiation() {
+        Square aSquare = new BasicSquare();
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = aSquare;
+        Board board = new Board(grid);
+        assertThat(board.squareAt(0,0)).isEqualTo(aSquare);
+    }
+
+    @Test
+    void testSquareAtWithNullSquareInitiation() {
+        Square[][] grid = new Square[1][1];
+        Board board = new Board(grid);
+        assertThat(board.squareAt(0,0)).isEqualTo(grid[0][0]);
     }
 }
